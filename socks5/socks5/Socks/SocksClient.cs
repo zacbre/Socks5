9070a7294474c52154edef7b9bf30bb809e9bc5e
ThrDev <ThrDev@users.noreply.github.com>
2016-06-22 18:50:05 -0400

    Added outbound interface option.
    Added license.

--- socks5/socks5/Socks/SocksClient.cs
+++ socks5/socks5/Socks/SocksClient.cs
@@ -1,4 +1,22 @@
-﻿using System;
+﻿/*
+    Socks5 - A full-fledged high-performance socks5 proxy server written in C#. Plugin support included.
+    Copyright (C) 2016 ThrDev
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+using System;
 using System.Collections.Generic;
 using System.Text;
 using socks5.Plugin;
@@ -19,7 +37,7 @@ namespace socks5.Socks
         }
         private SocksRequest req1;
         public SocksRequest Destination { get { return req1; } }
-        public void Begin(int PacketSize, int Timeout)
+        public void Begin(IPAddress outboundInterface, int PacketSize, int Timeout)
         {
             Client.onClientDisconnected += Client_onClientDisconnected;
             List<AuthTypes> authtypes = Socks5.RequestAuth(this);
@@ -103,7 +121,7 @@ namespace socks5.Socks
 					}  
                 //Send Tunnel Data back.
                 SocksTunnel x = new SocksTunnel(this, req, req1, PacketSize, Timeout);
-                x.Open();
+                x.Open(outboundInterface);
             }
             else if (Authenticated == 2)
             {
@@ -120,7 +138,7 @@ namespace socks5.Socks
 					}  
                 //Send Tunnel Data back.
                 SocksSpecialTunnel x = new SocksSpecialTunnel(this, w, req, req1, PacketSize, Timeout);
-                x.Open();
+                x.Open(outboundInterface);
             }
         }
 
