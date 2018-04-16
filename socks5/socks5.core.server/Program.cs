b201b202a031627853fee5e74c162490be8ec2b6
Dmitry Vlasov <d.vlasov@tochka.com>
2018-04-16 21:58:21 +0500

    migrate to core

---
+++ socks5/socks5.core.server/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Net;
+using socks5.Plugin;
+using Socks5Test;
+
+namespace socks5.core.server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var server = new Socks5Server(IPAddress.Any, 1080);
+            PluginLoader.ChangePluginStatus(true, typeof(Auth));
+            server.Start();
+        }
+    }
+}
